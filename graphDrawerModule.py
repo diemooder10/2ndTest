b418d39f9cd3d5dfc11238900bbe8f1787cafec8
Diego <diemooder@korea.ac.kr>
2024-12-09 21:36:59 +0900

    년도를 선택 완전하게 됨, 한글 표시 고침

--- graphDrawerModule.py
+++ graphDrawerModule.py
@@ -1,11 +1,16 @@
 import dataModule
 import matplotlib.pyplot as plt
 from matplotlib import rc
+from matplotlib import font_manager
 
 #그래프를 그리는 함수 (과목 이름과 년 받음)
 def drawGraph(subject,year):
     #한글 잘 표시하려고 font를 바꾸기
     rc("font", family="AppleGothic")
+    try:
+        font_manager.findfont("AppleGothic", fallback_to_default=False) #맥에서 문제 없이 실행할 것입니다
+    except:
+        rc("font", family="Malgun Gothic") #윈도우스이면 지난 코드가 문제가 있을 거라서 이걸로 바꿈
 
     #데이터 얻기
     data = dataModule.dataGetter(year)
--- main.py
+++ main.py
@@ -9,35 +9,29 @@ def main():
 
     while True:
         try:
-            yearinput = int(input("연도를 입력하세요(2023년 데이터만 존재합니다) 또는 0(종료): "))
+            yearinput = int(input("연도를 입력하세요(2020,2021,2022, 2023) 또는 0(종료): "))
         except ValueError:
             print("유효하지 않은 입력입니다. 연도를 숫자로 입력해주세요.")
             continue
 
-        # 유효한 연도 확인
-        if yearinput == 2023:
-            subject = input("과목명을 입력하세요 (예: 국어, 수학, 윤리와 사상) 또는 '종료': ").strip()
-            
-            
-            # 데이터 가져오기 및 그래프 그리기
-            try:
-                result = graphDrawerModule.drawGraph(subject, yearinput)
-                if result:
-                    print(result)  # "해당 과목이 없습니다." 출력
-            except ValueError as e:
-                print(f"데이터 처리 중 오류가 발생했습니다: {e}")
-            except FileNotFoundError:
-                print("필요한 데이터 파일이 존재하지 않습니다. 파일 경로를 확인하세요.")
-            except Exception as e:
-                print(f"예상치 못한 오류가 발생했습니다: {e}")
-        
-        
-        elif yearinput == 0:
+        if yearinput == 0:
             print("프로그램을 종료합니다.")
             break
         
-        else:
-            print("연도를 다시 입력해주세요. 현재 2023년 데이터만 지원됩니다.")
+        subject = input("과목명을 입력하세요 (예: 국어, 수학, 윤리와 사상) 또는 '종료': ").strip()
+        
+        
+        # 데이터 가져오기 및 그래프 그리기
+        try:
+            result = graphDrawerModule.drawGraph(subject, yearinput)
+            if result:
+                print(result)  # "해당 과목이 없습니다." 출력
+        except ValueError as e:
+            print(f"데이터 처리 중 오류가 발생했습니다: {e}")
+        except FileNotFoundError:
+            print("필요한 데이터 파일이 존재하지 않습니다. 파일 경로를 확인하세요.")
+        except Exception as e:
+            print(f"예상치 못한 오류가 발생했습니다: {e}")
 
 
 if __name__ == "__main__":
